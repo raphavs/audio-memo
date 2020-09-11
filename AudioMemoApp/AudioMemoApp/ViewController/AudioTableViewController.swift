419316333a42975be9b630d01598046c5bc493ab
Rapha <raphaelvosseler@web.de>
2020-09-11 14:32:05 +0200

    project audio-memo created

---
+++ AudioMemoApp/AudioMemoApp/ViewController/AudioTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioMemoApp
+//
+//  Created by Rapha on 11.09.20.
+//  Copyright © 2020 Rapha. All rights reserved.
+//
+
+import UIKit
+
+class AudioTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AudioMemoApp/AudioMemoApp/ViewController/RecordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecordViewController.swift
+//  AudioMemoApp
+//
+//  Created by Rapha on 11.09.20.
+//  Copyright © 2020 Rapha. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
